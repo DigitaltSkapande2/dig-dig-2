9146762c3b0d42df9c6925260c578290d87cf4fb
iFrexsim <47043939+Frexsim@users.noreply.github.com>
2025-06-04 23:09:21 +0200

    Add debug notes

---
+++ Assets/Scripts/DebugNotes/DebugNoteBillboard.cs
@@ -0,0 +1,44 @@
+using TMPro;
+using UnityEngine;
+
+public class DebugNoteBillboard : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI titleText;
+	[SerializeField] private TextMeshProUGUI noteText;
+	[SerializeField] private float unfocusedScale = 0.5f;
+	[SerializeField] private Color unfocusedColor = new(1f, 1f, 1f, 0.5f);
+	[SerializeField] private float animationSpeed = 5f;
+
+	private float focusAnimationTarget = 0;
+	private float focusAnimationProgress = 1;
+
+
+
+	private void Awake()
+	{
+		SetFocused(false);
+	}
+
+	private void Update()
+	{
+		if (focusAnimationProgress != focusAnimationTarget)
+		{
+			focusAnimationProgress = Mathf.Lerp(focusAnimationProgress, focusAnimationTarget, Time.deltaTime * animationSpeed);
+
+			transform.localScale = Vector3.Lerp(Vector3.one * unfocusedScale, Vector3.one, focusAnimationProgress);
+			titleText.color = Color.Lerp(unfocusedColor, Color.white, focusAnimationProgress);
+			noteText.color = Color.Lerp(unfocusedColor, Color.white, focusAnimationProgress);
+		}
+	}
+
+	public void ApplyNoteData(DebugNoteData debugNoteData)
+	{
+		titleText.text = debugNoteData.title;
+		noteText.text = debugNoteData.note;
+	}
+
+	public void SetFocused(bool newFocusValue)
+	{
+		focusAnimationTarget = newFocusValue ? 1 : 0;
+	}
+}
