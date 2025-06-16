f1e4e10ede84e591c7129692564b114f83a39091
DoktorDonald <164651385+DoktorDonald@users.noreply.github.com>
2025-06-16 20:33:42 +0200

    Attack Tweaks

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -147,7 +147,7 @@ public class PlayerAttack : MonoBehaviour, GameInputSystem.IAttackActions
     {
         HandleAiming();
 
-        Collider[] others = Physics.OverlapSphere(transform.position + attackDirection, 1, LayerMask.GetMask("Damageable"));
+        Collider[] others = Physics.OverlapSphere(transform.position + attackDirection, 1);
 
         Debug.Log(others.Length);
 
@@ -157,6 +157,8 @@ public class PlayerAttack : MonoBehaviour, GameInputSystem.IAttackActions
             {
                 UnityEngine.Debug.Log(other.name);
 
+                if (other.GetComponent<Damageable>() == null) { return; }
+                
                 other.gameObject.GetComponent<Damageable>().Damage(1);
             }
         }
