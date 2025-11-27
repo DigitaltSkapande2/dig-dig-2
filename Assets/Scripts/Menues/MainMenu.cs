fbdf51829ed6aaff278599c184f0827e7888a6e6
Alinto-1337 <121405632+Alinto-1337@users.noreply.github.com>
2025-11-28 00:04:58 +0100

    progress kinda... -_-

--- Assets/Scripts/Menues/MainMenu.cs
+++ Assets/Scripts/Menues/MainMenu.cs
@@ -4,6 +4,7 @@ using Cysharp.Threading.Tasks;
 using Mirror;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace DigDig2
 {
@@ -14,24 +15,30 @@ namespace DigDig2
         [SerializeField] Canvas mainMenuCanvas;
         [SerializeField] GameObject mainMenuContainer;
         [SerializeField] GameObject multiplayerLobbyPrefab;
+        [SerializeField] string multiplayerLobbySceneName;
 
 
-        public void StartHost()
+        public async void StartHost()
         {
             mainMenuContainer.SetActive(false);
-            Instantiate(multiplayerLobbyPrefab, mainMenuCanvas.transform);
-            Debug.Log("Spawned multiplayerlobby, Starting Host...");
-            OurNetworkManager.singleton.StartHost();
-            Debug.Log("Host started.");
+            Debug.Log("Starting Host...");
+
+            var netManager = OurNetworkManager.singleton;
+            netManager.StartHost();
+
+            await UniTask.WaitUntil(() => NetworkServer.active);
+
+            await SceneManager.LoadSceneAsync(multiplayerLobbySceneName);
+            Debug.Log("StartHost complete!!");
         }
 
         public void StartJoin()
         {
 
             mainMenuContainer.SetActive(false);
-            Instantiate(multiplayerLobbyPrefab, mainMenuCanvas.transform);
-            Debug.Log("Spawned multiplayerlobby, Starting Client...");
             OurNetworkManager.singleton.StartClient();
+
+            
             Debug.Log("Client started.");
         }
         
--- Assets/Scripts/Menues/MultiplayerLobby.cs
+++ Assets/Scripts/Menues/MultiplayerLobby.cs
@@ -1,7 +1,6 @@
-using System.Linq;
+using System.Text;
 using Mirror;
 using TMPro;
-using Unity.AppUI.UI;
 using UnityEngine;
 
 namespace DigDig2
@@ -11,45 +10,117 @@ namespace DigDig2
         [SerializeField] int targetPlayerCount = 2;
         [SerializeField] TMP_Text lobbyPlayerListText;
 
-        [SyncVar] int[] serverConnectionIDs;
+        [SerializeField] bool verboseLogging = false;
 
-        void Start()
+        // Use a SyncList so the server can modify the list and it will be synced to clients.
+        public readonly SyncList<int> serverConnectionIDs = new SyncList<int>();
+
+#region Server-side Init
+
+
+        private void Start()
         {
-            if (NetworkServer.active)
+            if (OurNetworkManager.singleton != null)
             {
-                Die();
+                OurNetworkManager.singleton.OnServerConnectAction += OnServerConnect;
+                OurNetworkManager.singleton.OnServerDisconnectAction += OnServerDisconnect;
             }
 
-            if (isServer) OurNetworkManager.singleton.OnServerConnectAction += OnServerConnect;
-        }
-
-        void Update()
-        {
+            if (!NetworkServer.active) return;
+            if (isServer) UpdateServerConnectionIDs();
             UpdateConnectionsListText();
         }
 
-        [Server]
-        public void OnServerConnect(NetworkConnectionToClient conn)
+        private void OnDestroy()
         {
-            serverConnectionIDs = NetworkServer.connections.Keys.ToArray();
+            if (OurNetworkManager.singleton != null)
+            {
+                OurNetworkManager.singleton.OnServerConnectAction -= OnServerConnect;
+                OurNetworkManager.singleton.OnServerDisconnectAction -= OnServerDisconnect;
+            }
         }
+
+#endregion
+#region Client-side Init
+        
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            Log("OnClient Start");
+            // subscribe to list changes so UI updates automatically when the server changes the list
+            serverConnectionIDs.Callback += OnServerConnectionIDsChanged;
+            UpdateConnectionsListText();
+        }
+
+        public override void OnStopClient()
+        {
+            base.OnStopClient();
+            serverConnectionIDs.Callback -= OnServerConnectionIDsChanged;
+        }
+
+#endregion
+#region Sync Communication
+
+        [Server]
+        void OnServerConnect(NetworkConnectionToClient conn)
+        {
+            Log("OnServerConnect");
+            UpdateServerConnectionIDs();
+        }
+
+        [Server]
+        void OnServerDisconnect(NetworkConnectionToClient conn)
+        {
+            Log("OnServerDisconnect");
+            UpdateServerConnectionIDs();
+        }
+
+        [Server]
+        void UpdateServerConnectionIDs()
+        {
+            Log("OnServer Update Connection IDs");
+            serverConnectionIDs.Clear();
+            foreach (var kv in NetworkServer.connections)
+            {
+                serverConnectionIDs.Add(kv.Key);
+            }
+
+            UpdateConnectionsListText();
+        }
+
+        void OnServerConnectionIDsChanged(SyncList<int>.Operation op, int index, int oldItem, int newItem)
+        {
+            Log("connections uppdated");
+            UpdateConnectionsListText();
+        }
+
+#endregion
+#region Misc
 
         void UpdateConnectionsListText()
         {
-            string gay = "";
+            if (lobbyPlayerListText == null) return;
+
+            if (serverConnectionIDs == null || serverConnectionIDs.Count == 0)
+            {
+                lobbyPlayerListText.text = "No players";
+                return;
+            }
+
+            var sb = new StringBuilder();
             foreach (int clID in serverConnectionIDs)
             {
-                gay += $"{clID.ToString()}\n";
+                sb.AppendLine(clID.ToString());
             }
-            lobbyPlayerListText.text = gay;
+
+            lobbyPlayerListText.text = sb.ToString();
         }
 
-
-        private void Die()
+        void Log(string msg) 
         {
-            gameObject.SetActive(false);
-            Destroy(gameObject);
+            Debug.Log(msg);
         }
 
+#endregion
     }
 }
