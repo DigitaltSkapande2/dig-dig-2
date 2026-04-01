72f6966169945ce1127c616964d6d9260a5c4e0e
DoktorDonald <164651385+DoktorDonald@users.noreply.github.com>
2026-04-01 14:04:09 +0200

    ...

---
+++ Assets/Scripts/Util/DayCycle.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace DigDig2
+{
+    [RequireComponent(typeof(Volume))]
+    public class DayCycle : MonoBehaviour
+    {
+        Volume volume;
+        ShadowsMidtonesHighlights smh;
+        [SerializeField] float dayDurationInMinutes;
+        [SerializeField] Vector4 nightValue;
+        private Vector4 startValue; 
+
+        void Awake( )
+        {
+            volume = GetComponent<Volume>( );
+
+            if ( !volume.profile.TryGet( out smh ) )
+            {
+                smh = volume.profile.Add<ShadowsMidtonesHighlights>();
+            }
+
+            startValue = smh.midtones.value;
+        }
+
+        void Update()
+        {
+            float lerpValue = (Mathf.Cos(Mathf.PI * Time.time / (dayDurationInMinutes * 30)) + 1) / 2;
+
+            smh.midtones.value = Vector4.Lerp(startValue, nightValue, lerpValue);
+        }
+    }
+}
